57d2eb196aadd86d5fd5b5d43be48ac1e2d1581a
Aakash Rajur <aakashrajur16@gmail.com>
2018-05-27 00:29:07 +0530

    imdb routes are insecure

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import LOADING_BACKGROUND from './assets/wall-e.jpg';
 import App from './components/App';
 import Loading from './components/Loading'
 import store from './redux';
 import registerServiceWorker from './registerServiceWorker';
 import {constructRoute, preloadImage} from "./utils";
 import {MOVIE_URL, ROOT_APP, ROOT_URL, TOP_10} from "./utils/constants";
-import LOADING_BACKGROUND from './assets/wall-e.jpg';
 
 async function initializeApp() {
 	await preloadImage(LOADING_BACKGROUND);
--- src/redux/movies.js
+++ src/redux/movies.js
@@ -1,16 +1,18 @@
 import {preloadImage} from "../utils";
-import {API_KEY, API_URL, FETCH_MOVIES, TOP_10} from "../utils/constants";
+import {FETCH_MOVIES, TOP_10} from "../utils/constants";
 import {dequeueLoading, enqueueLoading} from "./loading";
 
 const MOVIE_LOADING = 'MOVIE_LOADING';
 const IMAGE_LOADING = 'IMAGE_LOADING';
 
 async function getMovie({movieID: ID, desktop, mobile}) {
-	let response = await fetch(`${API_URL}?${API_KEY}&i=${ID}`);
+	/*let response = await fetch(`${API_URL}?${API_KEY}&i=${ID}`);
 	if (!response) throw new Error('No Response!');
 	if (response.status !== 200) throw new Error(response.statusText);
-	response = await response.json();
-	let {Ratings: [imd, rt, mc]} = response;
+	response = await response.json();*/
+	
+	let response = await import(`../assets/api/${ID}.json`),
+		{Ratings: [imd, rt, mc]} = response;
 	imd.Value = Number(imd.Value.replace(/(\d*)\/(\d*)/g, '$1')) / 10;
 	rt.Value = Number(rt.Value.replace(/(\d*)(%)/g, '$1')) / 100;
 	mc.Value = Number(mc.Value.replace(/(\d*)\/(\d*)/g, '$1')) / 100;
